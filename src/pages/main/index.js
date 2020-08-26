168f33ee7a9054b964da08e1bbc90c63b1a46ff1
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-08-26 12:58:54 -0300

    Implementação da paǵina main. A página consome os posters das séries e já tem os links configurados para levar para a página show.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
+
+import "./styles.css";
 
 export default class Main extends Component {
+  state = {
+    shows: [],
+  };
+
+  componentDidMount() {
+    this.loadMovies();
+  }
+
+  loadMovies = async () => {
+    const response = await api.get("/shows");
+    console.log(this.state.shows);
+    this.setState({ shows: response.data });
+    console.log(this.state.shows[0].name);
+  };
+
   render() {
-    return <h1>Main</h1>;
+    const { shows } = this.state;
+
+    return (
+      <div className="shows-list">
+        {shows.map((shows) => (
+          <Link to={`/show/${shows.id}`}>
+            <article key={shows.id}>
+              <img src={shows.image.medium} alt={shows.name} />
+            </article>
+          </Link>
+        ))}
+      </div>
+    );
   }
 }
